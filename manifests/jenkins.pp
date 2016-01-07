2fc8cf6a6268086ccc53281d96fd7b3ac1c497c8
Johan De Wit <johan@koewacht.net>
2016-01-07 07:43:29 +0000

    added gitlab-plugin and deps

--- manifests/jenkins.pp
+++ manifests/jenkins.pp
@@ -3,15 +3,18 @@ class profiles::jenkins {
   include git::client
   include jenkins
   
-  # first step, experiment with git/github
+  # gitlab-plugin and dependencies
 
-  jenkins::plugin { 'gitlab-plugin': }
-
-  #jenkins::plugin { ['promoted-builds', 'credentials', 'git-client',
-  #                     'scm-api', 'mailer', 'token-macro', 'matrix-project',
-  #                     'ssh-credentials', 'parameterized-trigger' ]: }
-
-  #jenkins::plugin { 'github': }
-  #jenkins::plugin { [ 'plain-credentials', 'github-api' ]: }
+  jenkins::plugin { 'git':
+    version =>  '2.4.1',
+  }
+    
+  jenkins::plugin { 'git-client':
+    version =>  '1.19.1',
+  }
+  
+  jenkins::plugin { 'gitlab-plugin':
+    version => '1.1.28',
+  }
 
 }
