fd527225d53ca87a70bd276377bbe23a77ff5085
Johan De Wit <johan@koewacht.net>
2016-01-07 11:41:44 +0000

    added param and validation

--- manifests/gitserver.pp
+++ manifests/gitserver.pp
@@ -1,8 +1,16 @@
-class profiles::gitserver {
+class profiles::gitserver (
+  $version = 'ce',
+) {
+
+  if !($version in [ 'ce', 'ee' ]) {
+    $validated_version = 'ce'
+  } else {
+    $validated_version = $version
+  }
 
   class { 'gitlab':
     external_url => 'http://gitlab.koewacht.net',
-    edition      => 'ce',
+    edition      => $validated_version,
   }
 
 }
