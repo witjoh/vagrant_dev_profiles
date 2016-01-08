ab205b66a0199e01e4c21c2d2a9844e90718a4f8
Puppet Module Manager <puppet@koewacht.net>
2016-01-08 13:55:34 +0000

    Added plugin dependencies

--- manifests/jenkins.pp
+++ manifests/jenkins.pp
@@ -7,14 +7,26 @@ class profiles::jenkins {
 
   jenkins::plugin { 'git':
     version =>  '2.4.1',
+    before  => Jenkins::plugin['git-client'],
   }
     
   jenkins::plugin { 'git-client':
     version =>  '1.19.1',
+    before  => Jenkins::plugin['gitlab-plugin'],
   }
   
   jenkins::plugin { 'gitlab-plugin':
     version => '1.1.28',
+  }
+
+  jenkins::plugin { 'matrix-projects':
+    version => latest,
+    before  => Jenkins::plugin['warnings'],
+  }
+  
+  jenkins::plugin { 'analysis-core':
+    version => latest,
+    before  => Jenkins::plugin['warnings'],
   }
   
   jenkins::plugin { 'warnings':
