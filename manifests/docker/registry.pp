d9a865c2f5506605bbef0cdacc502b5f73b04991
Johan De Wit <johan@koewacht.net>
2017-09-13 09:00:19 +0200

    remove the expose attribute

--- manifests/docker/registry.pp
+++ manifests/docker/registry.pp
@@ -3,7 +3,6 @@ class profiles::docker::registry {
   docker::run { 'registry':
     image            => 'registry:2',
     ports            => ['5000:5000'],
-    expose           => ['5000:5000'],
     extra_parameters => [ '--restart=always' ],
   }
 }
