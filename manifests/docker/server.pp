051086aa34cff68059e46c1b2ca8ebbc5da915a2
Johan De Wit <johan@koewacht.net>
2017-09-29 10:14:08 +0200

    force package name

--- manifests/docker/server.pp
+++ manifests/docker/server.pp
@@ -3,5 +3,6 @@ class profiles::docker::server {
   class { '::docker':
     docker_users                => [ 'vagrant' ],
     use_upstream_package_source => false,
+    package_name                => 'docker',
   }
 }
