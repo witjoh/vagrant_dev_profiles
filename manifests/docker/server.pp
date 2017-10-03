bfd365fd0f862ba1c2dfd1f34428782bad57db98
Johan De Wit <johan@koewacht.net>
2017-10-03 14:35:55 +0200

    workarounf for missing docker-runc

--- manifests/docker/server.pp
+++ manifests/docker/server.pp
@@ -6,4 +6,14 @@ class profiles::docker::server {
     package_name                => 'docker',
     docker_group                => 'dockerroot',
   }
+
+  # bug in rpm .... 
+  # /usr/bin/docker-current: Error response from daemon: shim error: docker-runc not installed on system.
+  #
+
+  file { '/usr/libexec/docker/docker-runc':
+    ensure  => link,
+    target  => '/usr/libexec/docker/docker-runc-current',
+    require => Class['docker'],
+  }
 }
