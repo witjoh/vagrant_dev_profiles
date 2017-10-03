11ee56eeb8210da61d5d1f7f5e020b131406af41
Johan De Wit <johan@koewacht.net>
2017-10-03 15:19:34 +0200

    only managing packages now

--- manifests/docker/server.pp
+++ manifests/docker/server.pp
@@ -1,19 +1,28 @@
 class profiles::docker::server {
 
-  class { '::docker':
-    docker_users                => [ 'vagrant' ],
-    use_upstream_package_source => false,
-    package_name                => 'docker',
-    docker_group                => 'dockerroot',
-  }
+  #class { '::docker':
+  #  docker_users                => [ 'vagrant' ],
+  #  use_upstream_package_source => false,
+  #  package_name                => 'docker',
+  #  docker_group                => 'dockerroot',
+  #}
 
   # bug in rpm .... 
   # /usr/bin/docker-current: Error response from daemon: shim error: docker-runc not installed on system.
   #
 
+  pakage { 'docker':
+    ensure =>  present,
+  }
+
   file { '/usr/libexec/docker/docker-runc':
     ensure  => link,
     target  => '/usr/libexec/docker/docker-runc-current',
-    require => Class['docker'],
+    require => Package['docker'],
+  }
+
+  group { 'dockerroot':
+    members => ['vagrant'],
+    require => Package['docker'],
   }
 }
